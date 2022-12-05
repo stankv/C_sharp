baf146ea07b017edac9b6e237d80a6c7d5364a1d
Stan Bad <stankv@mail.ru>
2022-12-05 14:28:55 +0300

    Цикл For: А в степени В

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Урок 4.
+
+//Задача 25:
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int DegreeOfNumber(int A, int B)
+{
+    if (B == 0) return 1;
+    else if (B == 1) return A;
+    int result = 1;
+    for (int i = 1; i <= B; i++) result *= A;
+    return result;
+}
+
+
+Console.Write("Input number: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Input degree of number: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine($"{DegreeOfNumber(x, y)}");
