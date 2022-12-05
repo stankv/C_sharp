4b92d685ee35cc04689116dfb7f66c4652e46547
Stan Bad <stankv@mail.ru>
2022-12-05 15:34:53 +0300

    Цикл For: Генерация массива и вывод его элементов

---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Урок 4.
+
+//Задача 3:
+//Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+
+int[] DefArray(int n, int r)    // генерируем массив
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new Random().Next(0, r);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array, int n)    // выводим элементы массива в консоль
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int size = 8;    // размер массива
+int range = 100;    // диапазон (верхняя граница) генерации случайных чисел
+PrintArray(DefArray(size, range), size);
