cbf6cf305ba238842862848f6a049ca581cff926
Stan Bad <stankv@mail.ru>
2022-12-09 09:28:03 +0300

    Подсчет введенных положительных чисел

---
+++ lesson6/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Урок 6.
+
+//Задача 1:
+//Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void CountPosNumbers()
+{
+    string str_in, str_out = string.Empty;
+    int count = 0;
+    while(true)
+    {
+        Console.Write("Input number: ");
+        str_in = Console.ReadLine();
+        if (str_in == "" || str_in == " ")
+        {
+            Console.WriteLine("Numbers entered: " + str_out);
+            Console.WriteLine($"Positive numbers: {count}");
+            return;
+        }
+        if (int.Parse(str_in) > 0) count++;
+        str_out = str_out + " " + str_in;
+    }
+}
+
+CountPosNumbers();
