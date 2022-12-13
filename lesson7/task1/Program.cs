7e832a628a75a10393559506d9655e84afc06b1b
Stan Bad <stankv@mail.ru>
2022-12-13 13:02:22 +0300

    Думерные массивы: форматированный вывод

---
+++ lesson7/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Урок 7.
+
+//Задача 1:
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] DefArray(int m, int n, int begin, int end)
+{
+    double[,] arr = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++) arr[i,j] = new Random().NextDouble() * (end - begin) + begin;
+    }
+    return arr;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write($" {array[i,j], 6}  ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Число строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Число столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+Console.Write("Диапазон значений, от: ");
+int from = int.Parse(Console.ReadLine());
+Console.Write("Диапазон значений, до: ");
+int to = int.Parse(Console.ReadLine());
+
+PrintArray(DefArray(row, columns, from, to));
