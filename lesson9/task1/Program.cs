14cbdbcfcd3aa87523fca1439a9f53c94903bb61
Stan Bad <stankv@mail.ru>
2022-12-19 22:46:05 +0300

    Рекурсия: четные числа от M до N

---
+++ lesson9/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Урок 9.
+
+// Задача 1. Задайте значения M и N. Напишите программу, которая выведет все
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+
+void EvenNumbers(int begin, int end)
+{
+    if ((begin % 2) == 0) Console.Write($" {begin} ");
+    if (begin == end) return;
+    EvenNumbers(begin + 1, end);
+}
+
+
+Console.Write("Введите начальное значение: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите конечное значение: ");
+int N = int.Parse(Console.ReadLine());
+
+if (M > N) Console.WriteLine("Конечное значение должно быть больше начального!");
+else if (M == N && (M % 2 == 0)) Console.WriteLine(M);
+else if (M == N && (M % 2 != 0)) Console.WriteLine("Четных чисел нет!");
+else 
+{
+    EvenNumbers(M, N);
+    Console.WriteLine();
+}
